591c9128638aff30180c579b19ea9951d912a914
Ajay Jakate <ajay.jakate@gmail.com>
2023-07-12 23:51:25 -0500

    number of factors

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::env;
+
+fn get_prime_factors_count(n: u64) -> u32 {
+    let mut count = 0;
+    let mut num = n;
+    let mut i = 2;
+
+    while i <= num {
+        if num % i == 0 {
+            count += 1;
+            num /= i;
+        } else {
+            i += 1;
+        }
+    }
+
+    count
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let number: u64 = match args[1].parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Invalid number provided.");
+            return;
+        }
+    };
+
+    let prime_factors_count = get_prime_factors_count(number);
+    println!("{}", prime_factors_count);
+}
