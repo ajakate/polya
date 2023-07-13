b95ded1057e93f14f33553592b5a00022b8021f2
Ajay Jakate <ajay.jakate@gmail.com>
2023-07-13 00:04:17 -0500

    add tests

--- src/main.rs
+++ src/main.rs
@@ -32,3 +32,16 @@ fn main() {
     let prime_factors_count = get_prime_factors_count(number);
     println!("{}", prime_factors_count);
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prime_factors_count() {
+        assert_eq!(get_prime_factors_count(84), 4);
+        assert_eq!(get_prime_factors_count(12), 3);
+        assert_eq!(get_prime_factors_count(37), 1);
+    }
+}
